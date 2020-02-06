227079587fb9fdac76b887cb3253d11a1a918c17
vivek <vivek.koyani@gmail.com>
2020-02-06 23:42:11 +0530

    database connection

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const cors = require('cors');
 const app = express();
 
 const db = require('./conflig/db').Database;
+
+//database connection
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => {
+        console.log('Database Connection Successfull')
+    })
+    .catch((err) => {
+        console.log('Uneble to connect with the database', err)
+    });
